4a56e1cb646b166358fa9a95c983f14bb7d52c81
huangxiran <78681321+huangxiran@users.noreply.github.com>
2023-12-17 12:12:16 +0800

    new blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: '3hiの破站',
+  author: '3hi666',
+  email: 'hxrink@163.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: '闲话 闲聊 闲情逸志 评天 评地 评头论足 笔墨纸砚皆乐器 雕章琢句皆音符',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
